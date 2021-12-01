1df4751746653bde9d41ae2fb8aa7d04bc5aeafb
Nijatasgar99 <91114183+Nijatasgar99@users.noreply.github.com>
2021-12-01 04:21:33 +0400

    Add files via upload

---
+++ ob.js
@@ -0,0 +1,50 @@
+// const companyObjArray=[{
+
+//         make:"Toyota",
+//         model:"Prado",
+//         year:2014,
+//         color:"black"
+//     },
+//     {
+//         make:"Honda",
+//         model:"civic",
+//         year:2016,
+//         color:"blue"
+//     },
+//     {
+//         make:"Ford",
+//         model:"Mustang",
+//         year:1968,
+//         color:"red"
+//     }]
+//     let uniquecompanyObjArray=[...new Map(companyObjArray.map((item)=>[item["make"], item])).values()
+// ];console.log(uniquecompanyObjArray);
+
+// let cars=[{
+
+//     make:"Toyota",
+//     model:"Prado",
+//     year:2014,
+//     color:"black"
+// },
+// {
+//     make:"Honda",
+//     model:"civic",
+//     year:2016,
+//     color:"blue"
+// },
+// {
+//     make:"Ford",
+//     model:"Mustang",
+//     year:1968,
+//     color:"red"
+// }]
+                         //1// console.log(cars[0].make)
+//2//for(i=0;i<cars.length; i++){
+    // if(cars[i].year<2010){
+    //     document.write(cars[i].make+","+cars[i].model)
+//     }
+// }
+
+
+
